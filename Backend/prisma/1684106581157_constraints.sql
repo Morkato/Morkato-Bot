d8a7f9f7bac6a63ec695dd3e2f21a119a5a3215b
V1NI1313 <almeidamarcus163@gmail.com>
2023-05-14 21:56:09 -0300

    add(prisma)

---
+++ Backend/prisma/1684106581157_constraints.sql
@@ -0,0 +1,47 @@
+CREATE function array_is_valid(roles text[], re text) RETURNS boolean
+AS $$
+DECLARE
+  roles_len int := array_length(roles, 1);
+  i int := 1;
+BEGIN
+  IF roles_len = 0 THEN
+    RETURN TRUE;
+  END IF;
+
+  WHILE i <= roles_len LOOP
+    IF NOT (roles[i] ~ re) THEN
+      RETURN FALSE;
+    END IF;
+  END LOOP;
+  
+  RETURN TRUE;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
+
+CREATE FUNCTION check_required_roles(required_roles int, roles text[])  RETURNS boolean
+AS $$
+BEGIN
+  RETURN CASE WHEN required_roles > 0 AND required_roles <= array_length(roles, 1) THEN TRUE ELSE FALSE END;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
+
+
+ALTER TABLE guilds ADD CONSTRAINT check_id CHECK (id ~ '^[0-9]+$');
+
+ALTER TABLE arts ADD CONSTRAINT check_name CHECK (LENGTH(name) > 1 AND LENGTH(name) < 132 AND name ~ '^[^\d \n\t\v\r\f\b]+$');
+ALTER TABLE arts ADD CONSTRAINT check_embed_title CHECK (embed_title IS NULL OR LENGTH(embed_title) > 0 AND LENGTH(embed_title) <= 96);
+ALTER TABLE arts ADD CONSTRAINT check_embed_description CHECK (embed_description IS NULL OR LENGTH(embed_description) > 0 AND LENGTH(embed_description) < 4096);
+ALTER TABLE arts ADD CONSTRAINT check_url CHECK (embed_url IS NULL OR embed_url ~ '^(https?://|cdn:/)[^\d \n\t\v\r\f\b]+$');
+ALTER TABLE arts ADD CONSTRAINT check_role CHECK (role IS NULL OR (role ~ '^[0-9]+$'));
+
+ALTER TABLE attacks ADD CONSTRAINT check_name CHECK (LENGTH(name) > 1 AND LENGTH(name) < 132 AND name ~ '^[^\d \n\t\v\r\f\b]+$');
+ALTER TABLE attacks ADD CONSTRAINT check_roles CHECK (array_is_valid(roles, '^0-9$'));
+ALTER TABLE attacks ADD CONSTRAINT check_required_exp CHECK (check_required_roles(required_roles, roles));
+ALTER TABLE attacks ADD CONSTRAINT check_damage CHECK (damage >= 0);
+ALTER TABLE attacks ADD CONSTRAINT check_stamina CHECK (stamina >= 0);
+ALTER TABLE attacks ADD CONSTRAINT check_embed_title CHECK (embed_title IS NULL OR LENGTH(embed_title) > 0 AND LENGTH(embed_title) <= 96);
+ALTER TABLE attacks ADD CONSTRAINT check_embed_description CHECK (embed_description IS NULL OR LENGTH(embed_description) > 0 AND LENGTH(embed_description) < 4096);
+ALTER TABLE attacks ADD CONSTRAINT check_url CHECK (embed_url IS NULL OR embed_url ~ '^(https?://|cdn:/)[^\d \n\t\v\r\f\b]+$');
+
+ALTER TABLE attacks_fields ADD CONSTRAINT check_text CHECK (LENGTH(text) > 0 AND LENGTH(text) <= 132 );
+ALTER TABLE attacks_fields ADD CONSTRAINT check_required_exp CHECK (array_is_valid(roles, '^[0-9]+$'));
---
+++ errors/__init__.py
@@ -0,0 +1,36 @@
+from discord.embeds import Embed
+from typing import Optional
+
+class BaseError(Exception):
+  def __init__(self, message: str, action: str, embeds: Optional[list[Embed]]) -> None:
+    super().__init__(message, action)
+
+    self.message = message
+    self.action = action
+    self.embeds = embeds
+
+class NotFoundError(BaseError):
+  def __init__(self, message: Optional[str] = None, action: Optional[str] = None, embeds: Optional[list[Embed]] = None) -> None:
+    message = message or "Não foi possivel achar esse item."
+    action = action or "Tente novamente com outro nome."
+    embeds = embeds or []
+
+    super().__init__(message, action, embeds)
+
+class AlrearyExistsError(BaseError):
+  def __init__(self, message: Optional[str] = None, action: Optional[str] = None, embeds: Optional[list[Embed]] = None) -> None:
+    message = message or "Esse item já existe."
+    action = action or "Tente novamente com outro nome."
+    embeds = embeds or []
+
+    super().__init__(message, action, embeds)
+
+class InternalServerError(BaseError):
+  def __init__(self, error: Exception, message: Optional[str] = None, action: Optional[str] = None, embeds: Optional[list[Embed]] = None) -> None:
+    message = message or "Erro interno."
+    action = action or "Tente novamente com outro nome."
+    embeds = embeds or []
+
+    super().__init__(message, action, embeds)
+
+    self.error = error
