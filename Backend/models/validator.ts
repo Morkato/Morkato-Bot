59bf8f9a30f986743d5dc06a10e727cdde2ecacb
V1NI1313 <almeidamarcus163@gmail.com>
2023-05-28 01:15:54 -0300

    up(validator) context

--- Backend/models/validator.ts
+++ Backend/models/validator.ts
@@ -1,8 +1,25 @@
+import utils from 'utils'
 import Joi from 'joi'
 
 import {
   ValidationError
 } from 'errors'
+
+export function makeContextSchemaField(
+  field: Joi.AnySchema,
+  original: any,
+  required?: boolean
+) {
+  if(required) {
+    return field.required();
+  }
+
+  if(original === undefined) {
+    field = field.default(original);
+  }
+
+  return field.optional();
+}
 
 export function makeContextSchema<
   S extends Record<string, Joi.AnySchema> = {},
@@ -14,7 +31,7 @@ export function makeContextSchema<
     required?: Partial<Record<Ks, boolean>>
   }
 ) {
-
+  return Joi.object(utils.object.map(schema, ([key, field]) => makeContextSchemaField(field, options?.default[key], options?.required[key])))
 }
 
 export default function validator<T, 
@@ -34,6 +51,18 @@ export default function validator<T,
     throw new ValidationError({ message: "O body tem que ser um Json.", action: "Tente enviar um Json dessa vez." })
   }
 
-  
+  const schemaFiltered = makeContextSchema(schema, options)
 
+  const { value, error } = schemaFiltered.validate(obj)
+
+  if(error) {
+    throw new ValidationError({
+      message: error.details[0].message,
+      key: error.details[0].context.key || error.details[0].context.type || 'object',
+      errorLocationCode: 'MODEL:VALIDATOR:SCHEMA',
+      type: error.details[0].type
+    });
+  }
+
+  return value as T;
 }
