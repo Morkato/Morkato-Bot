7e0ca6b57e4bbd5dba7508f1b85c0c6ba946f67e
V1NI1313 <almeidamarcus163@gmail.com>
2023-06-14 02:41:20 -0300

    fix: prisma schema

--- app.cpp
+++ app.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
-#include <thread>
+#include <cstdlib>
+#include <thread
 
 int main()
 {
+  std::thread bot([]()
+                  { std::system("python3 Index.py"); });
+  std::thread backend([]()
+                      { std::system("sudo yarn next:dev"); });
+
+  backend.join();
+  bot.join();
+
   return 0;
 }
