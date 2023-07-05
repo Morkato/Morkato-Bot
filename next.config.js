49d87d52d70f32182faa36096bfacda533a98ba9
V1NI1313 <almeidamarcus163@gmail.com>
2023-07-05 18:13:25 -0300

    fix: guild dynamic route

--- next.config.js
+++ next.config.js
@@ -3,14 +3,12 @@ require('dotenv').config();
 
 /**@type { import('next').NextConfig } */
 module.exports = {
-  serverRuntimeConfig: {
-    port: process.env.NEXT_PUBLIC_WEBSERVER_PORT || 5500
-  },
-  i18n: {
-    locales: ["en", "pt-BR"],
-    defaultLocale: "en"
-  },
-  experimental: {
-    appDir: true
-  },
+  async rewrites() {
+    return [
+      {
+        source: '/guilds/:guild_id/:path*',
+        destination: '/app/guilds/[guild_id]/route',
+      },
+    ];
+  }
 }
