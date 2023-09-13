9f2fd89f0d9aab36d8647af20ecb843d81eb9cf0
V1NI1313 <almeidamarcus163@gmail.com>
2023-09-13 09:58:35 -0300

    refactor: recharge utils art

--- commands/flags/v2/test.py
+++
@@ -1,6 +0,0 @@
-from morkato.utils.flag import FlagGroup, flag
-
-class TestGroup(FlagGroup):
-  @flag(aliases=['c'])
-  async def create(self, ctx) -> None:
-    await ctx.send('Ok, o novo sistema v2 das flags funcionou!')
