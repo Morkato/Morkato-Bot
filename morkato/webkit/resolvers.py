dc9c26cb5b33e0fa9e52ae9b3ea8b54461cdc388
V1NI1313 <almeidamarcus163@gmail.com>
2023-11-19 00:02:00 -0300

    refactor: tree webkit

--- morkato/webkit/resolvers.py
+++ morkato/webkit/resolvers.py
@@ -1,3 +1,18 @@
-from .src.resolvers import (
-  json
+__all__ = (
+  'json',
 )
+
+from typing import (
+  Callable,
+  Coroutine,
+  
+  Any
+)
+
+from .response import Response
+
+def json(decode: str) -> Callable[[Response], Coroutine[Any, Any, Any]]:
+  async def resolver(res: Response) -> Coroutine[Any, Any, Any]:
+    return await res.json()
+  
+  return resolver
