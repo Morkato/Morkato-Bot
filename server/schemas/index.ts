e0f74977a13ba56aeb5752082439cc32c538328b
V1NI1313 <almeidamarcus163@gmail.com>
2023-11-18 23:49:32 -0300

    feat: schemas for validate models

--- server/schemas/index.ts
+++ server/schemas/index.ts
@@ -1,180 +1,226 @@
-import { ValidationError } from 'errors'
+import { ValidationError } from 'morkato/errors'
 
 import Joi from 'joi'
 
-function makeContext(schema: Joi.AnySchema, required: boolean) {
-  if (required) {
-    return schema.required();
-  }
-
-  return schema;
+function uint() {
+  return Joi.number().integer().min(-1).max(4294967295)
 }
 
-type AssertParams<T = any> = { obj: Record<string, unknown>, schema: Joi.ObjectSchema<T> }
-
-type Props = {
-  key:       string
-  required?: boolean
-}
-
-type ObjectSchemaFunction = Record<string, (props: Props) => Joi.AnySchema> 
-
-type SchemaPropsParams<
-  ExtendSchema extends ObjectSchemaFunction = {}
-> = Record<string, { key: keyof typeof schemas | keyof ExtendSchema, required?: boolean }>
-
-export function getSchemaFromProps<ExtendParams extends ObjectSchemaFunction = {}>(props: SchemaPropsParams<ExtendParams>, extend?: ExtendParams) {
-  extend = extend ?? {} as ExtendParams
-
-  const allowed_schemas = Object.assign<typeof schemas, ExtendParams>(schemas, extend)
-
-  const notResolvedSchema = Object.entries(props).map(([key, prop]) => {
-    const callback = allowed_schemas[prop.key]
-
-    return [key, callback({ key, required: prop.required })];
-  })
-
-  const schema = Joi.object(Object.fromEntries(notResolvedSchema)).required().min(1).messages({
-    'object.base': `O Body deve ser do tipo "Json.Object"`,
-    'object.min': `O Objeto enviado deve ter no mínimo 1 chave.`
-  })
-
-  return schema;
-}
-
-export function assert<T>({ obj, schema }: AssertParams<T>) {
+export function validate(
+  object: Record<string, unknown>,
+  keys: Partial<Record<keyof typeof schemas, 'required' | 'optional'>>
+) {
   try {
-    obj = JSON.parse(JSON.stringify(obj))
-  } catch {
+    object = JSON.parse(JSON.stringify(object));
+  } catch (error) {
     throw new ValidationError({
       message: 'Não foi possível interpretar o valor enviado.',
       action: 'Verifique se o valor enviado é um JSON válido.',
-      errorLocationCode: 'SCHEMA:VALIDATOR:ERROR_PARSING_JSON',
-      key: 'obj'
+      errorLocationCode: 'MODEL:VALIDATOR:ERROR_PARSING_JSON',
+      key: 'object',
     })
   }
 
-  const { error, value } = schema.validate(obj)
+  let finalSchema = Joi.object().required().min(1).messages({
+    'object.base': `Body enviado deve ser do tipo Object.`,
+    'object.min': `Objeto enviado deve ter no mínimo uma chave.`,
+  })
 
-  if(error) {
+  for (const key of Object.keys(keys)) {
+    const callback = schemas[key]
+    finalSchema.concat(callback())
+  }
+
+  const { error, value } = finalSchema.validate(object, {
+    stripUnknown: true,
+    context: {
+      required: keys
+    }
+  })
+
+  if (error) {
     throw new ValidationError({
       message: error.details[0].message,
       key: error.details[0]?.context?.key || error.details[0]?.context?.type || 'object',
-      errorLocationCode: 'MODEL:VALIDATOR:SCHEMA',
-      type: error.details[0].type
-    });
+      errorLocationCode: 'MODEL:VALIDATOR:FINAL_SCHEMA',
+      type: error.details[0].type,
+    })
   }
 
   return value;
 }
 
-export function validate<T, ExtendParams extends ObjectSchemaFunction = {}>(obj: Record<string, unknown>, props: SchemaPropsParams<ExtendParams>, extend?: ExtendParams) {
-  const schema = getSchemaFromProps<ExtendParams>(props, extend)
-
-  return assert<T>({ obj, schema });
-}
-
-const schemas = {
-  id({ key, required = false }: Props) {
-    const schema = Joi
-      .string()
+export const schemas = {
+  exclude: () => Joi.object({
+    exclude: Joi.boolean()
+  }),
+  name: () => Joi.object({
+    name: Joi.string()
       .trim()
-      .min(1)
-      .regex(/^[0-9]+$/)
-      .when('$.required.id', { is: true, then: Joi.required(), otherwise: Joi.optional() })
-      .messages({
-        'any.required': `O campo "${key}" é obrigatório.`,
-        'string.empty': `O campo "${key}" não pode estar em branco.`,
-        'string.base': `O campo "${key}" deve ser do tipo string.`,
-        'string.regex': `O campo "${key}" deve seguir o regex: /^[0-9]+$/`
-      });
-
-    return makeContext(schema, required);
-  },
-  name({ key, required = false }: Props) {
-    const schema = Joi
-      .string()
-      .trim()
-      .min(1)
+      .min(3)
       .max(32)
-      .regex(/^[^-+>@&$].+[^-+>@&$]$/)
-      .messages({
-        'any.required': `O campo "${key}" é obrigatório.`,
-        'string.empty': `O campo "${key}" não pode estar em branco.`,
-        'string.base': `O campo "${key}" deve ser do tipo string.`,
-      });
-    
-    return makeContext(schema, required);
-  },
-  art_type({ key, required = false }: Props) {
-    const schema = Joi
-      .string()
-      .trim()
-      .valid('RESPIRATION', 'KEKKIJUTSU')
-
-    return makeContext(schema, required);
-  },
-  player_breed({ key, required = false }: Props) {
-    const schema = Joi
-      .string()
+      .regex(/^[^-+>@&$\s].+[^-+>@&$\s]$/)
+      .when('$required.name', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  breed: () => Joi.object({
+    breed: Joi.string()
       .trim()
       .valid('HUMAN', 'ONI', 'HYBRID')
-    
-    return makeContext(schema, required);
-  },
-  player_name({ key, required = false }: Props) {
-    const schema = Joi
-      .string()
+      .when('$required.breed', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  history: () => Joi.object({
+    history: Joi.string()
       .trim()
-      .valid('F', 'E', 'D', 'C', 'B', 'A', 'AA', 'AAA', 'AAAA', 'S', 'SS', 'SSS', 'SSSS')
-
-    return makeContext(schema, required);
-  },
-  dialog_choose({ key, required = false }: Props) {
-    const schema = Joi
+      .min(2)
+      .max(2048)
+      .regex(/^[^\s].*[^\s]$/)
+  }),
+  id: () => Joi.object({
+    id: Joi
       .string()
+      .min(6)
+      .regex(/^[0-9]+$/)
+      .when('$required.id', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  guild_id: () => Joi.object({
+    guild_id: Joi
+      .string()
+      .min(6)
+      .regex(/^[0-9]+$/)
+      .when('$required.guild_id', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  art_id: () => Joi.object({
+    art_id: Joi
+      .string()
+      .min(6)
+      .regex(/^[0-9]+$/)
+      .when('$required.art_id', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  item_id: () => Joi.object({
+    item_id: Joi
+      .string()
+      .min(6)
+      .regex(/^[0-9]+$/)
+      .when('$required.item_id', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  art_type: () => Joi.object({
+    type: Joi.string()
       .trim()
-      .valid('PLAYER', 'NPC')
-
-    return makeContext(schema, required);
-  },
-  embed_title({ key, required = false }: Props) {
-    const schema = Joi
+      .valid('RESPIRATION', 'KEKKIJUTSU', 'FIGHTING_STYLE')
+      .when('$required.art_type', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  parent_id: () => Joi.object({
+    parent_id: Joi
       .string()
+      .min(6)
+      .regex(/^[0-9]+$/)
+      .when('$required.parent_id', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  role: () => Joi.object({
+    role: Joi.string()
+      .min(6)
+      .regex(/^[0-9]+$/)
+      .when('$required.role', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  required_exp: () => Joi.object({
+    required_exp: uint()
+      .when('$required.required_exp', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  credibility: () => Joi.object({
+    credibility: uint()
+      .when('$required.credibility', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  damage: () => Joi.object({
+    damage: uint()
+      .when('$required.damage', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  life: () => Joi.object({
+    life: uint()
+      .when('$required.life', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  breath: () => Joi.object({
+    breath: uint()
+      .when('$required.breath', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  blood: () => Joi.object({
+    blood: uint()
+      .when('$required.blood', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  cash: () => Joi.object({
+    cash: uint()
+      .when('$required.cash', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  exp: () => Joi.object({
+    exp: uint()
+      .when('$required.exp', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  force: () => Joi.object({
+    force: uint()
+      .when('$required.force', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  resistance: () => Joi.object({
+    resistance: uint()
+      .when('$required.resistance', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  velocity: () => Joi.object({
+    velocity: uint()
+      .when('$required.velocity', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  stack: () => Joi.object({
+    stack: uint()
+      .min(1)
+      .max(4096)
+      .when('$required.stack', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  usable: () => Joi.object({
+    usable: Joi.boolean()
+      .when('$required.usable', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  embed_title: () => Joi.object({
+    embed_title: Joi.string()
       .trim()
       .allow(null)
       .min(1)
       .max(96)
-
-    return makeContext(schema, required);
-  },
-  embed_description({ key, required = false }: Props) {
-    const schema = Joi
-      .string()
+      .when('$required.embed_title', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  embed_description: () => Joi.object({
+    embed_description: Joi.string()
       .trim()
       .allow(null)
       .min(1)
       .max(4096)
-
-    return makeContext(schema, required);
-  },
-  embed_url({ key, required = false }: Props) {
-    const schema = Joi
-      .string()
+      .when('$required.embed_description', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  embed_url: () => Joi.object({
+    embed_url: Joi.string()
       .trim()
       .allow(null)
       .uri()
-
-    return makeContext(schema, required);
-  },
-  created_at({ key, required = false }: Props) {
-    const schema = Joi
-      .date()
-      .allow(Joi.string())
-
-      return makeContext(schema, required);
-  },
-  updated_at({ key, required = false }: Props) {
-    return schemas.created_at({ key, required });
-  }
+      .when('$required.embed_url', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  appearance: () => Joi.object({
+    appearance: Joi.string()
+      .trim()
+      .allow(null)
+      .uri()
+      .when('$required.appearance', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  banner: () => Joi.object({
+    banner: Joi.string()
+      .trim()
+      .allow(null)
+      .uri()
+      .when('$required.banner', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  created_at: () => Joi.object({
+    created_at: Joi.date()
+      .when('$required.created_at', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  }),
+  updated_at: () => Joi.object({
+    updated_at: Joi.date()
+      .when('$required.updated_at', { is: 'required', then: Joi.required(), otherwise: Joi.optional() })
+  })
 }
+
+export default validate;
