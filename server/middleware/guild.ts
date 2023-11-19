678a3a9cda27d0bda9680187e143da560b51700d
V1NI1313 <almeidamarcus163@gmail.com>
2023-11-18 23:54:26 -0300

    feat: added middleware for pages

---
+++ server/middleware/guild.ts
@@ -0,0 +1,31 @@
+import type { Handler as ExpressHandler, Request } from 'express'
+import type { Guild } from 'models/guild'
+import type { Handler } from './types'
+
+import { InternalServerError } from 'morkato/errors'
+
+import Guilds from 'models/guild'
+
+import client from 'morkato/infra/database'
+
+const guilds = Guilds(client.guild)
+
+export function getGuildID(req: Request) {
+  const value = req.params['guild_id']
+
+  if (!value) {
+    throw new InternalServerError({ message: "Erro interno no servidor <getGuildID>" })
+  }
+
+  return value;
+}
+
+export default function guild(handle: Handler<Guild>): ExpressHandler {
+  return async (req, res, next) => {
+    const id = getGuildID(req)
+
+    await handle(req, res, next, await guilds.get(id))
+  }
+}
+
+export { guild };
