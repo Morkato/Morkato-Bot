648cbe05125cf8ac1d6767169a05a0a697bad552
V1NI1313 <almeidamarcus163@gmail.com>
2023-11-18 23:43:13 -0300

    feat: snowflake IDS

--- server/utils/object.ts
+++ server/utils/object.ts
@@ -5,11 +5,20 @@ export namespace object {
   export function filter<AfterValue = any>(obj: Record<string, AfterValue>, filter: (props: [string, AfterValue]) => boolean) {
     return Object.fromEntries(Object.entries(obj).filter(([key, value]) => filter([key, value])))
   }
+  export function exclude<TBefore, Keys extends keyof TBefore>(obj: TBefore, keys: Array<Keys>): Omit<TBefore, Keys> {
+    return Object.fromEntries(Object.entries(obj).filter(([key, value]) => !keys.includes(key as Keys)))
+  }
 }
 
 export default object;
 
 export const {
   map,
-  filter
+  filter,
+  exclude
 } = object
+
+const a = exclude({
+  a: 1,
+  b: 2
+}, [ 'a' ])
